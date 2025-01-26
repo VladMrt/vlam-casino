4ab6a6e2661d4b466fe614223ca947d74e3c373b
VladMrt <handbal783@gmail.com>
2025-01-26 11:28:32 +0200

    da main

--- vlam-casino-main/main.py
+++ vlam-casino-main/main.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def mainPage():
-    return render_template('mainPage.html')
+    return render_template('Main Page.html')
+
+@app.route('/g')
+def geekWheel():
+    return render_template('geekWheel.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
